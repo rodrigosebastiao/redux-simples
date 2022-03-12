b5d0a504f572d5e363763b4bfab444824776036e
Rodrigo Sebastiao <rodrigo.silva@seliafullservice.com.br>
2022-03-11 21:31:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Card";
+import Intervalo from "./components/Intervalo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux</h1>
+      <div className="linha">
+        <Intervalo>X</Intervalo>
+      </div>
+      <div className="linha">
+        <Card title="Card 2" green>X</Card>
+        <Card title="Card 3" blue>X</Card>
+        <Card title="Card 4" purple>X</Card>
+      </div>
     </div>
   );
 }
